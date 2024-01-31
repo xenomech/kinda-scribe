e117c8f98013199f408e8044433f87b3f1bd3c68
xenomech <gokulsmenon227@gmail.com>
2024-01-31 17:29:05 +0530

    feat: 0th point package fixes

--- src/configs/editor.ts
+++ src/configs/editor.ts
@@ -1,15 +1,16 @@
 import { CloseTool } from '@/components/compounds/editor';
 
 import Code from '@editorjs/code';
+import { ToolConstructable, ToolSettings } from '@editorjs/editorjs';
 import InlineCode from '@editorjs/inline-code';
-import LinkTool from '@editorjs/link';
 import List from '@editorjs/list';
 import Table from '@editorjs/table';
 import Underline from '@editorjs/underline';
 import Strikethrough from '@sotaproject/strikethrough';
 
-const tools = {
-  linkTool: LinkTool,
+const tools:{
+  [toolName: string]: ToolConstructable|ToolSettings;
+} = {
   list: List,
   code: Code,
   inlineCode: InlineCode,
